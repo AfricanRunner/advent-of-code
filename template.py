e7cdbc0d1b288c97072516952686c3958694d1d9
AfricanRunner <me@africanrunner.net>
2022-11-29 21:28:56 -0800

    Initial commit

---
+++ template.py
@@ -0,0 +1,14 @@
+import sys
+
+
+def main(input: list[str]):
+    print(input)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("Invalid arguments, expected input file.")
+    input_file_path = sys.argv[1]
+    with open(input_file_path) as input_file:
+        input = [line.strip() for line in input_file.readlines()]
+        main(input)
