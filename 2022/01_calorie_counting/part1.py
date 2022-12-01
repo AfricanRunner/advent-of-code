ab8d9c3f05c18dbedefbf34d9fa765ae8c43a731
AfricanRunner <me@africanrunner.net>
2022-12-01 07:10:09 -0800

    Simplify solution to day 1: Calorie Counting

--- 2022/01_calorie_counting/part1.py
+++ 2022/01_calorie_counting/part1.py
@@ -1,22 +1,10 @@
 import sys
 
 
-def sum_calories(calories: list[str]) -> list[int]:
-    totals = []
-    current_total = 0
-    for item in calories:
-        if item == '':
-            totals.append(current_total)
-            current_total = 0
-        else:
-            current_total += int(item)
-    totals.append(current_total)
-    return totals
-
-
-def main(input: list[str]):
-    calories = sum_calories(input)
-    print(max(calories))
+def main(input: str):
+    calorie_totals = [sum([int(item) for item in group.split()])
+                      for group in input.split('\n\n')]
+    print(max(calorie_totals))
 
 
 if __name__ == '__main__':
@@ -24,5 +12,4 @@ if __name__ == '__main__':
         sys.exit("Invalid arguments, expected input file.")
     input_file_path = sys.argv[1]
     with open(input_file_path) as input_file:
-        input = [line.strip() for line in input_file.readlines()]
-        main(input)
+        main(input_file.read())
--- 2022/01_calorie_counting/part2.py
+++ 2022/01_calorie_counting/part2.py
@@ -1,23 +1,11 @@
 import sys
 
 
-def sum_calories(calories: list[str]) -> list[int]:
-    totals = []
-    current_total = 0
-    for item in calories:
-        if item == '':
-            totals.append(current_total)
-            current_total = 0
-        else:
-            current_total += int(item)
-    totals.append(current_total)
-    return totals
-
-
-def main(input: list[str]):
-    total_calories = sum_calories(input)
-    total_calories.sort()
-    print(total_calories[-1] + total_calories[-2] + total_calories[-3])
+def main(input: str):
+    calorie_totals = [sum([int(item) for item in group.split()])
+                      for group in input.split('\n\n')]
+    calorie_totals.sort()
+    print(calorie_totals[-1] + calorie_totals[-2] + calorie_totals[-3])
 
 
 if __name__ == '__main__':
@@ -25,5 +13,4 @@ if __name__ == '__main__':
         sys.exit("Invalid arguments, expected input file.")
     input_file_path = sys.argv[1]
     with open(input_file_path) as input_file:
-        input = [line.strip() for line in input_file.readlines()]
-        main(input)
+        main(input_file.read())
