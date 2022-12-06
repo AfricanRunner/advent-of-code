c90b2fb23741cf7e2223847995ab34364b89b07a
AfricanRunner <me@africanrunner.net>
2022-12-05 21:23:54 -0800

    Add solution for day 6: Tuning Trouble

---
+++ 2022/06_tuning_trouble/part2.py
@@ -0,0 +1,26 @@
+import sys
+
+
+def contains_unique_characters(string: str) -> bool:
+    return len(set(string)) == len(string)
+
+
+def find_start_marker(stream: str, sequence_size: int) -> int:
+    for i in range(sequence_size, len(stream)):
+        if contains_unique_characters(stream[i - sequence_size:i]):
+            return i
+    return -1
+
+
+def main(input: list[str]):
+    for line in input:
+        print(find_start_marker(line, 14))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        sys.exit("Invalid arguments, expected input file.")
+    input_file_path = sys.argv[1]
+    with open(input_file_path) as input_file:
+        input = [line.strip() for line in input_file.readlines()]
+        main(input)
